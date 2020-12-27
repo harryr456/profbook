1ba5da29d5d504fa2673bfce06d8094417bff1e4
Harunesh Ramanujam <harunesh@Harry-MacBook-Air.local>
2020-12-28 11:54:48 +1300

    Added Cars App and Page Data into HomePage

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('services', views.services, name='services'),
-    path('contact', views.contact, name='contact')
+    path('contact', views.contact, name='contact'),
+    path('cars', views.cars, name='cars'),
 ]
